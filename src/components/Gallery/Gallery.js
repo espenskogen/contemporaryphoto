fa6bba58d1e48fcaff63d4067164f3916a69ef23
Espen Skogen <espen@skogen.org>
2016-03-29 23:15:11 +0100

    Moved gallery CSS into Gallery.scss

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,36 +2,38 @@
 import React, { Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Gallery.scss';
+
 import { Carousel } from 'react-responsive-carousel';
 
 class Gallery extends Component {
 
   render() {
     return (
+
       <Carousel showThumbs={false} showArrows={true} >
 
         <div>
-          <img src="assets/1.jpg" />
+          <img src="assets/gallery-1.jpg" />
           <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src="assets/2.jpeg" />
+          <img src="assets/gallery-2.jpg" />
           <p className="legend">Legend 2</p>
         </div>
         <div>
-          <img src="assets/3.jpeg" />
+          <img src="assets/gallery-3.jpg" />
           <p className="legend">Legend 3</p>
         </div>
         <div>
-          <img src="assets/4.jpeg" />
+          <img src="assets/gallery-4.jpg" />
           <p className="legend">Legend 4</p>
         </div>
         <div>
-          <img src="assets/5.jpeg" />
+          <img src="assets/gallery-5.jpg" />
           <p className="legend">Legend 5</p>
         </div>
         <div>
-          <img src="assets/6.jpeg" />
+          <img src="assets/gallery-6.jpg" />
           <p className="legend">Legend 6</p>
         </div>
       </Carousel>
